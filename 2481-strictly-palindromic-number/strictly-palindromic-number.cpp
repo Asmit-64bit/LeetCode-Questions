492dfc4d3850cf858bf162f4510808b0c6ecc3b1
Asmit Samanta <asmit9324@gmail.com>
2025-12-24 18:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.68%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
-    bool isStrictlyPalindromic(int n) {
-       return false; 
+    bool ispalindrome(string s)
+    {
+        for(int i = 0 ; i <s.size()/2;i++)
+        {
+            if(s[i] != s[s.size()-1-i])
+                return false;
+        }
+        return true;
+    }
+    string getbase(int n, int b)
+    {
+        string s="";
+        while(n>0)
+        {
+            int d = n%b;
+            s = (char)(d+'0') + s;
+            n/=b;
+        }
+        return s;
+    }
+    bool isStrictlyPalindromic(int n) 
+    {
+        for(int i=2 ; i <= n-2 ; i++)
+        {
+            if(!ispalindrome(getbase(n,i)))
+                return false;
+        }
+        return true;
     }
 };
