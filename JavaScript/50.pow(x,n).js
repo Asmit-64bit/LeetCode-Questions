3845db3f20bf0abcc8a267e540b257f0e1f2dc5a
Asmit-64bit <asmit9324@gmail.com>
2026-01-02 20:05:10 +0530

    2-1-26

--- JavaScript/50.pow(x,n).js
+++ JavaScript/50.pow(x,n).js
@@ -4,6 +4,11 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    let a=Math.pow(x,n);
-    return a;
+    let res = 1;
+        while (n) {
+            if (n % 2) res = n > 0 ? res * x : res / x;
+            x = x * x;
+            n/=2;
+        }
+        return res;
 };
