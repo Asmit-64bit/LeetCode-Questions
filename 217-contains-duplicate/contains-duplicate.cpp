c96029964b58f93f4041f15a2710bfd5d3b6dccb
Asmit Samanta <asmit9324@gmail.com>
2026-05-20 19:17:16 +0530

    Time: 69 ms (15.14%) | Memory: 92.9 MB (15.76%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>map;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+        for(auto x : nums){
+            if(map[x]>=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
