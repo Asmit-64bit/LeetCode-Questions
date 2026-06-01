b6908612ed76564f99672cbe5e0c4112c9b82a56
Asmit Samanta <asmit9324@gmail.com>
2026-06-02 01:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (54.05%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            char a = s.at(left);
+            char b = s.at(right);
+            if (a >= 'A' && a <= 'Z') a = (char)(a + 32);
+            if (b >= 'A' && b <= 'Z') b = (char)(b + 32);
+            if (!((a >= 'a' && a <= 'z') || (a >= '0' && a <= '9'))) {
+                left++;
+                continue;
+            }
+            if (!((b >= 'a' && b <= 'z') || (b >= '0' && b <= '9'))) {
+                right--;
+                continue;
+            }
+            if (a != b) return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
